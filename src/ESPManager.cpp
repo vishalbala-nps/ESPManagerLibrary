26125436d67f788d64b15220ad0c8850ee9cb068
Vishal <vishal@Balas-MacBook-Pro.local>
2025-10-26 18:16:34 +0530

    Blank message retain

--- src/ESPManager.cpp
+++ src/ESPManager.cpp
@@ -136,7 +136,7 @@ void ESPManager::mqttCallback(char* topic, byte* payload, unsigned int length) {
             }
         } else if (strcmp(action, "delete") == 0) {
             Serial.println("*em:Got delete command");
-            _instance->_mqttClient.publish(fullStatusTopic.c_str(), "", false);
+            _instance->_mqttClient.publish(fullStatusTopic.c_str(), "", true);
             delay(2000);
             _instance->_mqttClient.disconnect();
             Serial.println("*em:Disconnected from MQTT broker");
